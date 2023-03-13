b85f1ac281cb6d2a4841de9d54160c9c48fae15c
Ari Wijaya <ariwijaya.its@gmail.com>
2023-03-13 07:48:35 +0700

    create web socket

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -6,8 +6,23 @@ let connect = () => {
   socket.onopen = () => {
     console.log("Successfully Connected");
   }
+
+  socket.onmessage = msg => {
+    console.log(msg);
+  }
+
+  socket.onclose = event => {
+    console.log('Socket Closed Connection: ', event);
+  }
+
+  socket.onerror = error => {
+    console.log('Socket Error: ', error);
+  }
 }
 
-let sendMsg = () => {}
+let sendMsg = (msg: string) => {
+  console.log('sending msg: ', msg);
+  socket.send(msg);
+}
 
-export {connect, sendMsg}
+export { connect, sendMsg };
